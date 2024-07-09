edd147ecf83492b8a892364c4b013e1439d1ba65
xMeliiodaS <bahaa.abozlf57@gmail.com>
2024-07-09 14:10:03 +0300

    Added assert to the login

--- UnityStoreAutomation/test/test_login_page.py
+++ UnityStoreAutomation/test/test_login_page.py
@@ -18,15 +18,21 @@ class TestLoginPage(unittest.TestCase):
         self.home_page = HomePage(self.driver)
 
     def test_login_successful(self):
-        # Wait for the page to load
+        # Arrange
         self.home_page.click_on_account_button()
         time.sleep(2)
         self.home_page.click_on_sign_in_button()
 
+        # Act
         login_page = LoginPage(self.driver)
         login_page.fill_username_input(self.config["email"])
         login_page.fill_password_input(self.config["password"])
         login_page.click_on_sign_in_button()
+        time.sleep(5)
+
+        # Assert
+        self.assertEqual(self.driver.current_url, "https://assetstore.unity.com/")
+        self.driver.quit()
 
 
 if __name__ == "__main__":
