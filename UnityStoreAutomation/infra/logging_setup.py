e75e0588831cd7013ef30b8dc06b0d78547efe72
NOTxMeliiodaaS <kotarou.gaia557@gmail.com>
2024-07-14 12:11:13 +0300

    Changed the file name unity_store_logfile

--- UnityStoreAutomation/infra/logging_setup.py
+++ UnityStoreAutomation/infra/logging_setup.py
@@ -6,10 +6,10 @@ class LoggingSetup:
     A class to manage logging configuration for the project.
     """
     logging.basicConfig(
-        filename="../solar_logfile.log",
+        filename="../unity_store_logfile.log",
         level=logging.INFO,
         format='%(asctime)s : %(levelname)s : %(message)s',
-        force=True
+        force=False
     )
     # Suppress logging from 'undetected_chromedriver' to avoid clutter
     logging.getLogger('undetected_chromedriver').setLevel(logging.WARNING)
