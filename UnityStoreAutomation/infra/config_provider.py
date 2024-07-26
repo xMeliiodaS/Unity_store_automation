569a570efdd4614360f46088f70d55007dd6ecc5
xMeliiodaS <bahaa.abozlf57@gmail.com>
2024-07-26 09:54:35 +0300

    Created the jira handler

--- UnityStoreAutomation/infra/config_provider.py
+++ UnityStoreAutomation/infra/config_provider.py
@@ -19,3 +19,17 @@ class ConfigProvider:
                 return json.load(f)
         except FileNotFoundError:
             print(f"File not found. Starting with an empty library.")
+
+    @staticmethod
+    def load_secret_json():
+        """
+        Load the configuration from the specified JSON file.
+
+        :return: The loaded configuration as a dictionary.
+        """
+        try:
+            # Since we are using with modules with can't use relative path, so changed it to absolute path
+            with open('D:\\Users\\User\\Desktop\\Unity_store_automation\\UnityStoreAutomation\\secret.json', 'r') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            print(f"File not found. Starting with an empty library.")
